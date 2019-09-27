6e17c548d97de05d430bff26b73217f0bbe4d9da
Khairul Azmi <khairulfs21@gmail.com>
2019-09-27 10:22:35 +0800

    add migration,model controller

---
+++ database/migrations/2019_09_27_014804_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->string('title');
+            $table->longText('body');
+            $table->double('price');
+            $table->string('category')->comment('mobile,electronic,furniture,fashion');
+            $table->longText('image_path')->nullable();
+            $table->string('status')->default('available')->comment('available','reserved', 'sold');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
